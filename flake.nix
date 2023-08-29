40359c9b8be8c8090a6a8be48069eaa8ecceb29a
ppenguin <hieronymusv@gmail.com>
2023-08-29 20:21:01 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,37 @@
+# flake made with nix flake new -t "github:serokell/nix-templates#python-poetry2nix" xls2latex
+
+{
+  description = "Xls2latex converts xls(x) worksheets to LaTeX tables (best used with pandoc(omatic))";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        customOverrides = self: super: {
+          # Overrides go here
+        };
+
+        app = pkgs.poetry2nix.mkPoetryApplication {
+          projectDir = ./.;
+          overrides =
+            [ pkgs.poetry2nix.defaultPoetryOverrides customOverrides ];
+        };
+
+        packageName = "xls2latex";
+      in {
+        packages.${packageName} = app;
+
+        defaultPackage = self.packages.${system}.${packageName};
+
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ poetry ];
+          inputsFrom = builtins.attrValues self.packages.${system};
+        };
+      });
+}
