6cab9a92f777f2c9b790be91750baed29eb8bccf
ppenguin <hieronymusv@gmail.com>
2023-11-22 12:36:18 +0100

    flake update [broken]

--- flake.nix
+++ flake.nix
@@ -2,25 +2,30 @@
   description = "xls2latex converts xls(x) worksheets to LaTeX tables (best used with pandoc(omatic))";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = inputs@{ self, nixpkgs, flake-utils, poetry2nix }:
   {
     # Nixpkgs overlay providing the application
     overlay = nixpkgs.lib.composeManyExtensions [
       (final: prev: {
         xls2latex = import ./default.nix { pkgs = prev; };
-        xls2latex-devenv = prev.poetry2nix.mkPoetryEnv {
-          projectDir = ./.;
-          # overrides = [ prev.poetry2nix.defaultPoetryOverrides ];
-        };
+        # xls2latex-devenv = final.poetry2nix.mkPoetryEnv {
+        #   projectDir = ./.;
+        #   # overrides = [ prev.poetry2nix.defaultPoetryOverrides ];
+        # };
+        # poetry2nix = poetry2nix.packages.${prev.system};
       })
     ];
   } // flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; overlays = [ self.overlay ];};
+      inherit (poetry2nix.packages.${system}.poetry2nix) mkPoetryApplication mkPoetryEnv;
       packageName = "xls2latex";
     in
     {
@@ -29,7 +34,8 @@
 
       devShell = pkgs.mkShell {
         buildInputs = with pkgs;[
-          xls2latex-devenv
+          # xls2latex-devenv
+          (mkPoetryEnv { projectDir = self; }).env
           poetry
           (pkgs.python3.withPackages (p: with p; [ pylint ]))
         ];
