65fe0b3ad5aeb0c6cdddd786bf6c6e804dca98b1
ppenguin <hieronymusv@gmail.com>
2023-11-22 12:48:38 +0100

    improved poetry2nix flake

--- flake.nix
+++ flake.nix
@@ -2,44 +2,30 @@
   description = "xls2latex converts xls(x) worksheets to LaTeX tables (best used with pandoc(omatic))";
 
   inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     poetry2nix = {
       url = "github:nix-community/poetry2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, poetry2nix }:
-  {
-    # Nixpkgs overlay providing the application
-    overlay = nixpkgs.lib.composeManyExtensions [
-      (final: prev: {
-        xls2latex = import ./default.nix { pkgs = prev; };
-        # xls2latex-devenv = final.poetry2nix.mkPoetryEnv {
-        #   projectDir = ./.;
-        #   # overrides = [ prev.poetry2nix.defaultPoetryOverrides ];
-        # };
-        # poetry2nix = poetry2nix.packages.${prev.system};
-      })
-    ];
-  } // flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; overlays = [ self.overlay ];};
-      inherit (poetry2nix.packages.${system}.poetry2nix) mkPoetryApplication mkPoetryEnv;
-      packageName = "xls2latex";
-    in
-    {
-      packages.${system}.${packageName} = pkgs.xls2latex;
-      defaultPackage = pkgs.xls2latex;
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+      in
+      {
+        packages = {
+          xls2latex = mkPoetryApplication { projectDir = self; };
+          default = self.packages.${system}.xls2latex;
+        };
 
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs;[
-          # xls2latex-devenv
-          (mkPoetryEnv { projectDir = self; }).env
-          poetry
-          (pkgs.python3.withPackages (p: with p; [ pylint ]))
-        ];
-      };
-    }
-  );
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.xls2latex ];
+          packages = [ pkgs.poetry ];
+        };
+      });
 }
